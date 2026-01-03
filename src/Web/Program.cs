d9ca420d530cd9415ee55857e2a86c7ed88e06a2
DUCANH\Duc Anh <ducanh.nguyen.dev@gmail.com>
2026-01-03 23:51:20 +0700

    l

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -18,10 +18,11 @@ else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
 app.UseHealthChecks("/health");
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseSwaggerUi(settings =>
